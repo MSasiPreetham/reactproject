d095cf125093701a6902a7beee11531d75cd749c
sasi preetham <medarametlasasi@gmail.com>
2020-08-06 02:00:29 +0530

    Updates

---
+++ precache-manifest.8669d65f906b00230470a1dbb1ce28d7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f94e9cbf344d75a7661c4c01f13429a",
+    "url": "/reactproject/index.html"
+  },
+  {
+    "revision": "4ef366d3c96250630d5d",
+    "url": "/reactproject/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "534e0a88e4dcd045cf09",
+    "url": "/reactproject/static/js/2.785b15d9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactproject/static/js/2.785b15d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ef366d3c96250630d5d",
+    "url": "/reactproject/static/js/main.71af53d1.chunk.js"
+  },
+  {
+    "revision": "743b5b8e6a976900f131",
+    "url": "/reactproject/static/js/runtime-main.56d96f34.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/reactproject/static/media/logo.5d5d9eef.svg"
+  }
+]);
